3726129ffeef10564687138355c10803cb020d88
orviz <orviz@ifca.unican.es>
2022-11-04 16:53:36 +0100

    Update script.hilly-wisteria-crocodile.sh

--- .sqa/script.hilly-wisteria-crocodile.sh
+++ .sqa/script.hilly-wisteria-crocodile.sh
@@ -1,4 +1,4 @@
 (
 cd bitbucket.org/cessda/cessda.cvs.two &&
-    npx stylelint --config ~/.stylelintrc.json **/*.{js ts css vue}
+    npx stylelint --config ~/.stylelintrc.json "**/*.{js,ts,css,vue}"
 )
