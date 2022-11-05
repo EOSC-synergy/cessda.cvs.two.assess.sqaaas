c9ea20b63be86b5c9d593060bca3e8bb22a37508
orviz <orviz@ifca.unican.es>
2022-11-05 09:32:17 +0100

    Update script.flabby-denim-dachsbracke.sh

--- .sqa/script.flabby-denim-dachsbracke.sh
+++ .sqa/script.flabby-denim-dachsbracke.sh
@@ -1,4 +1,4 @@
 (
 cd bitbucket.org/cessda/cessda.cvs.two &&
-    npx stylelint --config-basedir . --config /root/.stylelintrc.json **/*.{js,ts,css,vue}
+    npx stylelint --config-basedir . --config /root/.stylelintrc.json "**/*.{js,ts,css,vue}"
 )
