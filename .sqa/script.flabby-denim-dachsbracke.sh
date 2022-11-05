078a8192d0c681985b3d15627d210677b34071c2
orviz <orviz@ifca.unican.es>
2022-11-05 09:01:50 +0100

    Add JePL file structure

---
+++ .sqa/script.flabby-denim-dachsbracke.sh
@@ -0,0 +1,4 @@
+(
+cd bitbucket.org/cessda/cessda.cvs.two &&
+    npx stylelint --config-basedir . --config /root/.stylelintrc.json **/*.{js,ts,css,vue}
+)
--- .sqa/script.sickly-chocolate-tetra.sh
+++
@@ -1,4 +0,0 @@
-(
-cd bitbucket.org/cessda/cessda.cvs.two &&
-    npx stylelint --config-basedir . --config /root/.stylelintrc.json **/*.{js ts css vue}
-)
