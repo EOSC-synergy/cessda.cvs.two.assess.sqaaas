1a5139901f303bbb6f83b3afc7b10f239cabf1ac
orviz <orviz@ifca.unican.es>
2022-12-22 13:49:50 +0100

    Add JePL file structure

--- .sqa/script.flabby-denim-dachsbracke.sh
+++
@@ -1,5 +0,0 @@
-(
-cd bitbucket.org/cessda/cessda.cvs.two &&
-    stylelint --version # &&
-    # npx stylelint --config-basedir . --config /root/.stylelintrc.json "**/*.{js,ts,css,vue}"
-)
--- .sqa/script.shaky-tan-oriole.sh
+++ .sqa/script.shaky-tan-oriole.sh
@@ -1,4 +1,4 @@
 (
 cd bitbucket.org/cessda/cessda.cvs.two &&
-    licensee detect . --json
+    licensee detect . --confidence 60 --json
 )
